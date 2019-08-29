c9dad3e12dd7a16ad18a9b8ef28e13f4f432439e
Hrissimir <hrisimir.dakov@gmail.com>
2019-08-29 23:44:17 +0300

    - added class base.jobs_page.JobsPage

---
+++ src/scrape_jobs/base/jobs_page.py
@@ -0,0 +1,114 @@
+import datetime
+from typing import List, Dict, Optional
+
+import pytz
+from hed_utils.support import log
+from hed_utils.support.time_tool import get_local_tz_name, get_local_datetime, convert_to_tz
+
+from scrape_jobs.base.job_result import JobResult
+
+JobsData = List[Dict[str, str]]
+
+
+class JobsPage:
+
+    @classmethod
+    def get_stop_datetime(cls, past_n_days: int, *, tz_name: Optional[str] = None) -> datetime.datetime:
+        tz_name = tz_name or get_local_tz_name()
+        local_datetime = get_local_datetime()
+        local_stop_datetime = local_datetime - datetime.timedelta(days=past_n_days)
+        return convert_to_tz(local_stop_datetime, tz_name)
+
+    @classmethod
+    def parse_datetime_string(cls,
+                              value: str, *,
+                              fmt="%Y-%m-%d",
+                              tz_name: Optional[str] = None) -> Optional[datetime.datetime]:
+        try:
+            tz_name = tz_name or get_local_tz_name()
+            tz = pytz.timezone(tz_name)
+            dt = datetime.datetime.strptime(value, fmt)
+            return tz.localize(dt)
+        except:
+            return None
+
+    def get_visible_jobs(self) -> List[JobResult]:
+        raise NotImplementedError()
+
+    def enter_search_details(self, query: str, location: str):
+        raise NotImplementedError()
+
+    def press_search_button(self):
+        raise NotImplementedError()
+
+    def wait_for_results(self):
+        raise NotImplementedError()
+
+    def view_next_results_page(self):
+        raise NotImplementedError()
+
+    def collect_most_recent_jobs(self,
+                                 query: str,
+                                 location: str,
+                                 past_n_days: int,
+                                 tz_name: Optional[str]) -> JobsData:
+        tz_name = tz_name or get_local_tz_name()
+        stop_datetime = self.get_stop_datetime(past_n_days, tz_name=tz_name)
+        log.info("collecting most recent jobs "
+                 "(query='%s', location='%s', past_n_days='%s', tz_name='%s', stop_datetime='%s')",
+                 query, location, past_n_days, tz_name, stop_datetime)
+
+        most_recent_jobs: JobsData = []
+
+        def is_new_job(job_data: Dict[str, str]):
+            same_jobs = [j for j in most_recent_jobs if j["url"] == job_data["url"]]
+            return not bool(same_jobs)
+
+        self.enter_search_details(query, location)
+        self.press_search_button()
+        self.wait_for_results()
+
+        current_pageno = 1
+        is_first_bad_page = True
+
+        while True:
+            log.info("processing jobs results on page #%s", current_pageno)
+            current_page_new_jobs = []
+
+            visible_jobs = self.get_visible_jobs()
+            log.info("got %s visible jobs on page %s", len(visible_jobs), current_pageno)
+
+            for current_job in visible_jobs:
+                current_job_data = current_job.as_dict(tz_name)
+
+                # skip jobs that were already collected
+                if not is_new_job(current_job_data):
+                    continue
+
+                # skip older jobs and jobs with no date
+                job_post_datetime = self.parse_datetime_string(current_job_data["date"], tz_name=tz_name)
+                if (not job_post_datetime) or (job_post_datetime.date() < stop_datetime.date()):
+                    continue
+
+                current_page_new_jobs.append(current_job_data)
+
+            is_bad_page = not (bool(current_page_new_jobs))
+
+            if is_bad_page:
+                log.info("page #%s was a bad page - it had no unseen jobs", current_pageno)
+                if is_first_bad_page:
+                    log.warning("this was first bad page, will stop results iteration on next occasion")
+                    is_first_bad_page = False
+                    current_pageno += 1
+                    continue
+                else:
+                    log.warning("page #%s was second bad page in a row - done with results iteration!", current_pageno)
+                    break
+            else:
+                is_first_bad_page = True
+                log.info("found %s unseen jobs on page #%s", len(current_page_new_jobs), current_pageno)
+                most_recent_jobs.extend(current_page_new_jobs)
+                current_pageno += 1
+
+        most_recent_jobs.sort(key=lambda j: self.parse_datetime_string(j["date"], tz_name=tz_name))
+        return most_recent_jobs

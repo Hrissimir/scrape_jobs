7a78eccc5d1d918263c2edcc50ebbc3b1ce06418
Hrissimir <hrisimir.dakov@gmail.com>
2019-08-29 14:03:43 +0300

    - fix for config read in seek scraper

--- src/scrape_jobs/sites/seek_com_au/scraper.py
+++ src/scrape_jobs/sites/seek_com_au/scraper.py
@@ -11,18 +11,18 @@ UploadParams = namedtuple("UploadParams", "name file sheet")
 
 
 def get_scrape_params(config: ConfigParser) -> ScrapeParams:
-    what = config[SeekComAu.WHAT]
-    where = config[SeekComAu.WHERE]
+    what = config.get(SeekComAu.KEY, SeekComAu.WHAT)
+    where = config.get(SeekComAu.KEY, SeekComAu.WHERE)
     days = config.getint(SeekComAu.KEY, SeekComAu.DAYS)
-    tz = config[SeekComAu.TIMEZONE]
+    tz = config.get(SeekComAu.KEY, SeekComAu.TIMEZONE)
     scrape_params = ScrapeParams(what, where, days, tz)
     log.debug("got scrape params: %s", scrape_params)
     return scrape_params
 
 
 def get_upload_params(config: ConfigParser) -> UploadParams:
-    name = config[Default.UPLOAD_SPREADSHEET_NAME]
-    file = config[Default.UPLOAD_SPREADSHEET_JSON]
+    name = config.get(Default.KEY, Default.UPLOAD_SPREADSHEET_NAME)
+    file = config.get(Default.KEY, Default.UPLOAD_SPREADSHEET_JSON)
     sheet = config.getint(SeekComAu.KEY, SeekComAu.UPLOAD_WORKSHEET_INDEX)
     upload_params = UploadParams(name, file, sheet)
     log.debug("got upload params: %s", upload_params)

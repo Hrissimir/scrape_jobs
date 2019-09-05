7ff7dd70846fe546d0d21f03bfda31fbe1807727
Hrissimir <hrisimir.dakov@gmail.com>
2019-09-05 06:10:21 +0300

    - deleted obsolete stuff from scrape_config

--- src/scrape_jobs/common/scrape_config.py
+++ src/scrape_jobs/common/scrape_config.py
@@ -2,7 +2,7 @@ import pkgutil
 from configparser import ConfigParser
 from io import StringIO
 from pathlib import Path
-from typing import Optional, List
+from typing import Optional, List, NoReturn
 from unittest import mock
 
 from hed_utils.support import log
@@ -102,32 +102,6 @@ class ScrapeConfig:
                                  DEFAULT_CONTENTS)
 
 
-class Default:
-    KEY = "DEFAULT"
-    UPLOAD_SPREADSHEET_NAME = "upload_spreadsheet_name"
-    UPLOAD_SPREADSHEET_JSON = "upload_spreadsheet_json"
-    UPLOAD_WORKSHEET_INDEX = "upload_worksheet_index"
-
-
-class SeekComAu:
-    KEY = "seek.com.au"
-    WHAT = "what"
-    WHERE = "where"
-    DAYS = "days"
-    TIMEZONE = "timezone"
-    UPLOAD_WORKSHEET_INDEX = "upload_worksheet_index"
-
-
-class LinkedinCom:
-    KEY = "linkedin.com"
-    KEYWORDS = "keywords"
-    LOCATION = "location"
-    DATE_POSTED = "date_posted"
-    DAYS = "days"
-    TIMEZONE = "timezone"
-    UPLOAD_WORKSHEET_INDEX = "upload_worksheet_index"
-
-
 def get_sample_config() -> ConfigParser:
     log.debug("sample config file contents:\n%s", DEFAULT_CONTENTS)
     config = ConfigParser(interpolation=None)
@@ -152,7 +126,7 @@ def format_config(config: ConfigParser) -> str:
         return fake_out.getvalue()
 
 
-def write_sample_config(file_path: Optional[str] = None):
+def write_sample_config(file_path: Optional[str] = None) -> NoReturn:
     log.debug("writing sample config file at: '%s'", file_path)
     if not file_path:
         file_path = str(Path.cwd().joinpath(DEFAULT_FILENAME))
@@ -162,7 +136,7 @@ def write_sample_config(file_path: Optional[str] = None):
         configfile.write(DEFAULT_CONTENTS.encode("utf-8"))
 
 
-def read_config(file_path: Optional[str] = None):
+def read_config(file_path: Optional[str] = None) -> ConfigParser:
     log.debug("reading config from file: '%s'", file_path)
     if not file_path:
         file_path = str(Path.cwd().joinpath(DEFAULT_FILENAME))
@@ -174,65 +148,3 @@ def read_config(file_path: Optional[str] = None):
 
     log.debug("done reading config from '%s' got:\n%s", file_path, format_config(config))
     return config
-
-
-def assert_valid_config(config):
-    assert isinstance(config, ConfigParser), \
-        f"The provided config was not a ConfigParser instance! ({type(config).__name__})"
-
-    try:
-        assert Default.KEY in config, f"No '[{Default.KEY}]' section in config file"
-        default = config[Default.KEY]
-
-        assert Default.UPLOAD_SPREADSHEET_NAME in default, \
-            f"No value for '{Default.UPLOAD_SPREADSHEET_NAME}=' in '[{Default.KEY}]' section"
-        assert Default.UPLOAD_SPREADSHEET_JSON in default, \
-            f"No value for '{Default.UPLOAD_SPREADSHEET_JSON}=' in '[{Default.KEY}]' section"
-        assert Default.UPLOAD_WORKSHEET_INDEX in default, \
-            f"No value for '{Default.UPLOAD_WORKSHEET_INDEX}=' in '[{Default.KEY}]' section"
-
-        assert SeekComAu.KEY in config, f"No '[{SeekComAu.KEY}]' section in config file"
-        seek = config[SeekComAu.KEY]
-
-        assert SeekComAu.WHAT in seek, \
-            f"No value for '{SeekComAu.WHAT}=' in '[{SeekComAu.KEY}]' section!"
-        assert SeekComAu.WHERE in seek, \
-            f"No value for '{SeekComAu.WHERE}=' in '[{SeekComAu.KEY}]' section!"
-        assert SeekComAu.DAYS in seek, \
-            f"No value for '{SeekComAu.DAYS}=' in '[{SeekComAu.KEY}]' section!"
-
-        assert SeekComAu.TIMEZONE in seek, \
-            f"No value for '{SeekComAu.TIMEZONE}=' in '[{SeekComAu.KEY}]' section!"
-
-        assert SeekComAu.UPLOAD_WORKSHEET_INDEX in seek, \
-            f"No value for '{SeekComAu.UPLOAD_WORKSHEET_INDEX}=' in '[{SeekComAu.KEY}]' section!"
-
-        assert LinkedinCom.KEY in config, f"No '[{LinkedinCom.KEY}]' section in config file"
-        linkedin = config[LinkedinCom.KEY]
-
-        assert LinkedinCom.KEYWORDS in linkedin, \
-            f"No value for '{LinkedinCom.KEYWORDS}=' in '[{LinkedinCom.KEY}]' section!"
-
-        assert LinkedinCom.LOCATION in linkedin, \
-            f"No value for '{LinkedinCom.LOCATION}=' in '[{LinkedinCom.KEY}]' section!"
-
-        assert LinkedinCom.DATE_POSTED in linkedin, \
-            f"No value for '{LinkedinCom.DATE_POSTED}=' in '[{LinkedinCom.KEY}]' section!"
-
-        assert LinkedinCom.DAYS in linkedin, \
-            f"No value for '{LinkedinCom.DAYS}=' in '[{LinkedinCom.KEY}]' section!"
-
-        assert LinkedinCom.TIMEZONE in linkedin, \
-            f"No value for '{LinkedinCom.TIMEZONE}=' in '[{LinkedinCom.KEY}]' section!"
-
-        assert LinkedinCom.UPLOAD_WORKSHEET_INDEX in linkedin, \
-            f"No value for '{LinkedinCom.KEYWORDS}=' in '[{LinkedinCom.KEY}]' section!"
-
-    except AssertionError as err:
-        lines = [f"Bad config ({err}, {err.args})! Ensure your file matches the following sample:",
-                 "\n-----------SAMPLE START-----------\n",
-                 format_config(get_sample_config()),
-                 "\n-----------SAMPLE END-----------\n"]
-        msg = "\n".join(lines)
-        new_err = AssertionError(msg)
-        raise new_err from err

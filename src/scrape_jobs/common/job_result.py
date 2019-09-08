1cf2cce378b54fb0a42f51a44a65150b25b53470
Hrissimir <hrisimir.dakov@gmail.com>
2019-09-08 11:45:54 +0300

    removed abc related stuff from JobResult

--- src/scrape_jobs/common/job_result.py
+++ src/scrape_jobs/common/job_result.py
@@ -1,4 +1,3 @@
-from abc import ABC, abstractmethod
 from datetime import datetime
 from typing import Optional, List
 
@@ -6,7 +5,7 @@ from bs4 import BeautifulSoup
 from hed_utils.selenium.page_objects.base.element_soup import ElementSoup
 
 
-class JobResult(ElementSoup, ABC):
+class JobResult(ElementSoup):
 
     def __init__(self, soup: BeautifulSoup):
         super().__init__(soup)
@@ -15,22 +14,17 @@ class JobResult(ElementSoup, ABC):
     def keys(cls) -> List[str]:
         return ["utc_datetime", "location", "title", "company", "url"]
 
-    @abstractmethod
     def get_utc_datetime(self) -> Optional[datetime]:  # pragma: no cover
-        pass
+        raise NotImplementedError()
 
-    @abstractmethod
     def get_location(self) -> Optional[str]:  # pragma: no cover
-        pass
+        raise NotImplementedError()
 
-    @abstractmethod
     def get_title(self) -> Optional[str]:  # pragma: no cover
-        pass
+        raise NotImplementedError()
 
-    @abstractmethod
     def get_company(self) -> Optional[str]:  # pragma: no cover
-        pass
+        raise NotImplementedError()
 
-    @abstractmethod
     def get_url(self) -> Optional[str]:  # pragma: no cover
-        pass
+        raise NotImplementedError()

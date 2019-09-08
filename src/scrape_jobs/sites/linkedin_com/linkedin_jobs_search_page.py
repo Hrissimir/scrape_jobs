6659783e5f30a66ef79e624aa8cacdaf8423eb8b
Hrissimir <hrisimir.dakov@gmail.com>
2019-09-08 12:28:10 +0300

    - moved LinkedinJobsSearchBar and LinkedinJobsSearchForm to linkedin_jobs_search_page

--- src/scrape_jobs/sites/linkedin_com/linkedin_jobs_search_page.py
+++ src/scrape_jobs/sites/linkedin_com/linkedin_jobs_search_page.py
@@ -1,12 +1,58 @@
+from typing import Tuple
+
 from hed_utils.selenium import driver
 from hed_utils.support import log
+from selenium.webdriver.common.by import By
 
 from scrape_jobs.common.jobs_search_page import JobsSearchPage
-from scrape_jobs.sites.linkedin_com.linkedin_jobs_search_bar import LinkedinJobsSearchBar
-from scrape_jobs.sites.linkedin_com.linkedin_jobs_search_form import LinkedinJobsSearchForm
 from scrape_jobs.sites.linkedin_com.linkedin_jobs_results_page import LinkedinJobsResultsPage
 from scrape_jobs.sites.linkedin_com.linkedin_search_filters import DatePostedFilter
 from scrape_jobs.sites.linkedin_com.linkedin_search_section import LinkedinSearchSection
+
+
+class LinkedinJobsSearchBar(LinkedinSearchSection):
+    @property
+    def keywords_input_locator(self) -> Tuple[str, str]:
+        return self.KEYWORDS_INPUT
+
+    @property
+    def location_input_locator(self) -> Tuple[str, str]:
+        return self.LOCATION_INPUT
+
+    @property
+    def search_button_locator(self) -> Tuple[str, str]:
+        return self.SEARCH_BUTTON
+
+    LOCATOR = By.XPATH, "//header//nav/section[@class='search-bar']"
+    KEYWORDS_INPUT = By.XPATH, LOCATOR[-1] + "//input[contains(@name,'keywords')]"
+    LOCATION_INPUT = By.XPATH, LOCATOR[-1] + "//input[contains(@name,'location')]"
+
+    SEARCH_BUTTON = By.CSS_SELECTOR, "section[data-searchbar-type='JOBS'] button[type='submit']"
+
+    def __init__(self):
+        super().__init__(self.LOCATOR)
+
+
+class LinkedinJobsSearchForm(LinkedinSearchSection):
+    @property
+    def keywords_input_locator(self) -> Tuple[str, str]:
+        return self.KEYWORDS_INPUT
+
+    @property
+    def location_input_locator(self) -> Tuple[str, str]:
+        return self.LOCATION_INPUT
+
+    @property
+    def search_button_locator(self) -> Tuple[str, str]:
+        return self.SEARCH_BUTTON
+
+    LOCATOR = By.XPATH, "//main//section//form[contains(@action,'/jobs/search')]"
+    KEYWORDS_INPUT = By.XPATH, LOCATOR[-1] + "//input[contains(@name,'keywords')]"
+    LOCATION_INPUT = By.XPATH, LOCATOR[-1] + "//input[contains(@name,'location')]"
+    SEARCH_BUTTON = By.XPATH, "//button[contains(@form,'JOBS')]"
+
+    def __init__(self):
+        super().__init__(self.LOCATOR)
 
 
 class LinkedinJobsSearchPage(JobsSearchPage):

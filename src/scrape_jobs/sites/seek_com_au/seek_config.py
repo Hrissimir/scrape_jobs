6f295ec8673390eed9c4bde827287b1d07e7ceec
Hrissimir <hrisimir.dakov@gmail.com>
2019-09-08 15:32:01 +0300

    - updated seek_config

--- src/scrape_jobs/sites/seek_com_au/seek_config.py
+++ src/scrape_jobs/sites/seek_com_au/seek_config.py
@@ -2,6 +2,13 @@ from configparser import ConfigParser
 from typing import List
 
 from scrape_jobs.common.scrape_config import ScrapeConfig
+from scrape_jobs.common.upload_config import UploadConfig
+
+
+class SeekUploadConfig(UploadConfig):
+    @classmethod
+    def get_section_name(cls) -> str:
+        return "seek.com.au"
 
 
 class SeekConfig(ScrapeConfig):
@@ -9,20 +16,20 @@ class SeekConfig(ScrapeConfig):
         super().__init__(cfg)
 
     @classmethod
-    def section_name(cls) -> str:
+    def get_section_name(cls) -> str:
         return "seek.com.au"
 
     @classmethod
-    def section_keys(cls) -> List[str]:
-        return super().section_keys() + ["what", "where"]
+    def get_section_keys(cls) -> List[str]:
+        return super().get_section_keys() + ["what", "where"]
 
     @property
     def what(self) -> str:
-        return self.config_section.get("what")
+        return self.get_section().get("what")
 
     @property
     def where(self) -> str:
-        return self.config_section.get("where")
+        return self.get_section().get("where")
 
     def get_search_params(self) -> dict:
         return dict(what=self.what,

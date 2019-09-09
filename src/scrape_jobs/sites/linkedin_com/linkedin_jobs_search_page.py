3f949d6843ac5fb78c0f0914a307445120e909e8
Hrissimir <hrisimir.dakov@gmail.com>
2019-09-09 23:41:44 +0300

    - removed unneeded methods from LinkedinJobsSearchPage - moved LinkedinJobsSearchForm and LinkedinJobsSearchBar to module linkedin_search_section - refactored  LinkedinSearchSection, LinkedinJobsSearchForm and LinkedinJobsSearchBar to improve testability

--- src/scrape_jobs/sites/linkedin_com/linkedin_jobs_search_page.py
+++ src/scrape_jobs/sites/linkedin_com/linkedin_jobs_search_page.py
@@ -1,58 +1,11 @@
-from typing import Tuple
-
 from hed_utils.selenium import driver
 from hed_utils.support import log
-from selenium.webdriver.common.by import By
 
 from scrape_jobs.common.jobs_search_page import JobsSearchPage
 from scrape_jobs.sites.linkedin_com.linkedin_jobs_results_page import LinkedinJobsResultsPage
 from scrape_jobs.sites.linkedin_com.linkedin_search_filters import DatePostedFilter
-from scrape_jobs.sites.linkedin_com.linkedin_search_section import LinkedinSearchSection
-
-
-class LinkedinJobsSearchBar(LinkedinSearchSection):
-    @property
-    def keywords_input_locator(self) -> Tuple[str, str]:
-        return self.KEYWORDS_INPUT
-
-    @property
-    def location_input_locator(self) -> Tuple[str, str]:
-        return self.LOCATION_INPUT
-
-    @property
-    def search_button_locator(self) -> Tuple[str, str]:
-        return self.SEARCH_BUTTON
-
-    LOCATOR = By.XPATH, "//header//nav/section[@class='search-bar']"
-    KEYWORDS_INPUT = By.XPATH, LOCATOR[-1] + "//input[contains(@name,'keywords')]"
-    LOCATION_INPUT = By.XPATH, LOCATOR[-1] + "//input[contains(@name,'location')]"
-
-    SEARCH_BUTTON = By.CSS_SELECTOR, "section[data-searchbar-type='JOBS'] button[type='submit']"
-
-    def __init__(self):
-        super().__init__(self.LOCATOR)
-
-
-class LinkedinJobsSearchForm(LinkedinSearchSection):
-    @property
-    def keywords_input_locator(self) -> Tuple[str, str]:
-        return self.KEYWORDS_INPUT
-
-    @property
-    def location_input_locator(self) -> Tuple[str, str]:
-        return self.LOCATION_INPUT
-
-    @property
-    def search_button_locator(self) -> Tuple[str, str]:
-        return self.SEARCH_BUTTON
-
-    LOCATOR = By.XPATH, "//main//section//form[contains(@action,'/jobs/search')]"
-    KEYWORDS_INPUT = By.XPATH, LOCATOR[-1] + "//input[contains(@name,'keywords')]"
-    LOCATION_INPUT = By.XPATH, LOCATOR[-1] + "//input[contains(@name,'location')]"
-    SEARCH_BUTTON = By.XPATH, "//button[contains(@form,'JOBS')]"
-
-    def __init__(self):
-        super().__init__(self.LOCATOR)
+from scrape_jobs.sites.linkedin_com.linkedin_search_section import LinkedinSearchSection, LinkedinJobsSearchForm, \
+    LinkedinJobsSearchBar
 
 
 class LinkedinJobsSearchPage(JobsSearchPage):
@@ -62,8 +15,7 @@ class LinkedinJobsSearchPage(JobsSearchPage):
 
     JOBS_RESULTS = LinkedinJobsResultsPage()
 
-    @property
-    def search_section(self) -> LinkedinSearchSection:
+    def get_search_section(self) -> LinkedinSearchSection:
         if self.JOBS_SEARCH_FORM.is_visible():
             return self.JOBS_SEARCH_FORM
         elif self.JOBS_SEARCH_BAR.is_visible():
@@ -71,34 +23,23 @@ class LinkedinJobsSearchPage(JobsSearchPage):
         else:
             raise RuntimeError("neither the search-form, nor the search-bar were present")
 
-    def set_keywords(self, keywords):
-        self.search_section.set_keywords(keywords)
-        driver.wait_until_page_loads()
-
-    def set_location(self, location):
-        self.search_section.set_location(location)
-        driver.wait_until_page_loads()
-
-    def set_date_posted(self, date_posted):
-        self.DATE_POSTED_FILTER.set_date_posted(date_posted)
-        driver.wait_until_page_loads()
-
     def trigger_search(self):
-        # no need to trigger - entering details re-triggers automatically
-
-        return
+        self.get_search_section().trigger_search()
 
     def set_search_params(self, **params):
         log.info("setting linkedin jobs search params: %s", params)
 
-        keywords = params.pop("keywords", "")
-        self.set_keywords(keywords)
+        keywords = params.pop("keywords", None)
+        if keywords:
+            self.get_search_section().set_keywords(keywords)
 
-        location = params.pop("location", "")
-        self.set_location(location)
+        location = params.pop("location", None)
+        if location:
+            self.get_search_section().set_location(location)
 
-        date_posted = params.pop("date_posted", DatePostedFilter.PAST_MONTH)
-        self.set_date_posted(date_posted)
+        date_posted = params.pop("date_posted", None)
+        if date_posted:
+            self.DATE_POSTED_FILTER.set_date_posted(date_posted)
 
     def wait_for_search_complete(self):
         driver.wait_until_visible_element(self.JOBS_RESULTS.LOCATOR)
--- src/scrape_jobs/sites/linkedin_com/linkedin_search_section.py
+++ src/scrape_jobs/sites/linkedin_com/linkedin_search_section.py
@@ -1,31 +1,33 @@
-from abc import ABC, abstractproperty
 from typing import Tuple
 
 from hed_utils.selenium import driver
 from hed_utils.selenium.page_objects.base.page_element import PageElement
 from hed_utils.support import log
+from selenium.webdriver.common.by import By
 
 
-class LinkedinSearchSection(PageElement, ABC):
-    @abstractproperty
-    def keywords_input_locator(self) -> Tuple[str, str]:
-        pass
+class LinkedinSearchSection(PageElement):
 
-    @abstractproperty
-    def location_input_locator(self) -> Tuple[str, str]:
-        pass
-
-    @abstractproperty
-    def search_button_locator(self) -> Tuple[str, str]:
-        pass
+    def __init__(self,
+                 locator: Tuple[str, str],
+                 keywords_input_locator: Tuple[str, str],
+                 location_input_locator: Tuple[str, str],
+                 search_button_locator: Tuple[str, str]
+                 ):
+        super().__init__(locator)
+        self.keywords_input_locator = keywords_input_locator
+        self.location_input_locator = location_input_locator
+        self.search_button_locator = search_button_locator
 
     def set_keywords(self, keywords):
         log.info("setting search keywords to: '%s'", keywords)
         driver.set_text(self.keywords_input_locator, keywords, press_enter=True)
+        driver.wait_until_page_loads()
 
     def set_location(self, location):
         log.info("setting search location to: '%s'", location)
         driver.set_text(self.location_input_locator, location, press_enter=True)
+        driver.wait_until_page_loads()
 
     def trigger_search(self):
         log.info("triggering search...")
@@ -33,3 +35,29 @@ class LinkedinSearchSection(PageElement, ABC):
         driver.scroll_into_view(self.search_button_locator)
         driver.wait_until_page_loads()
         driver.click_element(self.search_button_locator)
+
+
+class LinkedinJobsSearchForm(LinkedinSearchSection):
+    LOCATOR = By.XPATH, "//main//section//form[contains(@action,'/jobs/search')]"
+
+    KEYWORDS_INPUT = By.XPATH, LOCATOR[-1] + "//input[contains(@name,'keywords')]"
+
+    LOCATION_INPUT = By.XPATH, LOCATOR[-1] + "//input[contains(@name,'location')]"
+
+    SEARCH_BUTTON = By.XPATH, "//button[contains(@form,'JOBS')]"
+
+    def __init__(self):
+        super().__init__(self.LOCATOR, self.KEYWORDS_INPUT, self.LOCATION_INPUT, self.SEARCH_BUTTON)
+
+
+class LinkedinJobsSearchBar(LinkedinSearchSection):
+    LOCATOR = By.XPATH, "//header//nav/section[@class='search-bar']"
+
+    KEYWORDS_INPUT = By.XPATH, LOCATOR[-1] + "//input[contains(@name,'keywords')]"
+
+    LOCATION_INPUT = By.XPATH, LOCATOR[-1] + "//input[contains(@name,'location')]"
+
+    SEARCH_BUTTON = By.CSS_SELECTOR, "section[data-searchbar-type='JOBS'] button[type='submit']"
+
+    def __init__(self):
+        super().__init__(self.LOCATOR, self.KEYWORDS_INPUT, self.LOCATION_INPUT, self.SEARCH_BUTTON)
